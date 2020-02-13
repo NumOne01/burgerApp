e1f401ad3dc7b59578c6d5814e752173d75b78d6
Alireza <alireza.fz96@gmail.com>
2020-02-13 20:37:32 +0330

    add state to BuildBurger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component, Fragment } from "react"
 import Burger from "../../components/Burger/Burger"
 
 export default class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 0,
+			meat: 0,
+			cheese: 0,
+			bacon: 0
+		}
+	}
 	render() {
 		return (
 			<Fragment>
-				<Burger />
+				<Burger ingredients={this.state.ingredients} />
 			</Fragment>
 		)
 	}
