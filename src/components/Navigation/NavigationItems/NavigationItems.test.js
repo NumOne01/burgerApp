fd3dd1e683505a065115ac2946a36cfda7cace5b
Alireza <alireza.fz96@gmail.com>
2020-02-26 22:56:43 +0330

    added test unit

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationItems from './NavigationItems'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+configure({ adapter: new Adapter() })
+
+describe('<NavigationItems>', () => {
+	it('should render two navigationItems if the user is not authenticated', () => {
+		const wrapper = shallow(<NavigationItems />)
+		expect(wrapper.find(NavigationItem)).toHaveLength(2)
+	})
+})
